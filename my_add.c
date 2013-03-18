7c59024e286297e814c5421813ad430d6d9be51f
raskanskyz <raskanskyz@gmail.com>
2013-03-18 18:25:44 +0200

    moved function into folder

---
+++ my_add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*function prototype*/
+unsigned int my_add(unsigned int a, unsigned int b);
+
+int main()
+{
+	unsigned int my_add(unsigned int a, unsigned int b)
+	}
+
+	}/*my_add*/
+}/*main*/
+
