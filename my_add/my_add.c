0259d367b72d392b51379b2eed1492c7cf91fe89
Eran Raskansky <raskanskyz@gmail.com>
2013-03-21 16:44:52 +0200

    recursive binaryCout function added

--- my_add/my_add.c
+++ my_add/my_add.c
@@ -2,12 +2,24 @@
 
 /*function prototype*/
 unsigned int my_add(unsigned int a, unsigned int b);
+unsigned int binaryCount(unsigned int n);
+
 
 int main()
 {
-    my_add(500,12);
+    unsigned int a=0;
+    unsigned int b=0;
+    printf("Please enter two numbers to sum:");
+    scanf("%u%u",&a, &b);
+   int result = my_add(a, b);
+    printf("%d + %d (base 10) = %d\n", a, b, result);
+    printf("%d + %d (base 2) = ", a, b);
+    binaryCount(result);
+    printf("\n");
+
     return 0;
 }/*main*/
+
 unsigned int my_add(unsigned int a, unsigned int b)
 {
     int mask = 0, result = 0;
@@ -24,6 +36,23 @@ unsigned int my_add(unsigned int a, unsigned int b)
         
     }/*while*/
     
-    printf("result is: %d\n", result);
-    return 0;
+    return result;
 }/*my_add*/
+
+unsigned int binaryCount(unsigned int n){
+    
+    if (n<2 && n%2==0) {
+        
+        return;
+        
+    }/*base*/
+    
+    else{
+        
+        binaryCount(n/2);
+        
+        printf("%d",n%2);
+        
+    }/*else*/
+    
+}/*binaryCount*/
