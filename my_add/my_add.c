580a4bdaa93c1e650f1a96aa1c13f7b83027033d
Eran Raskansky <raskanskyz@gmail.com>
2013-03-21 16:53:51 +0200

    change binaryCount to 'void'

--- my_add/my_add.c
+++ my_add/my_add.c
@@ -2,7 +2,7 @@
 
 /*function prototype*/
 unsigned int my_add(unsigned int a, unsigned int b);
-unsigned int binaryCount(unsigned int n);
+void binaryCount(unsigned int n);
 
 
 int main()
@@ -11,12 +11,12 @@ int main()
     unsigned int b=0;
     printf("Please enter two numbers to sum:");
     scanf("%u%u",&a, &b);
-   int result = my_add(a, b);
+    int result = my_add(a, b);
     printf("%d + %d (base 10) = %d\n", a, b, result);
     printf("%d + %d (base 2) = ", a, b);
     binaryCount(result);
     printf("\n");
-
+    
     return 0;
 }/*main*/
 
@@ -39,11 +39,10 @@ unsigned int my_add(unsigned int a, unsigned int b)
     return result;
 }/*my_add*/
 
-unsigned int binaryCount(unsigned int n){
+void binaryCount(unsigned int n){
     
     if (n<2 && n%2==0) {
-        
-        return;
+        ;
         
     }/*base*/
     
