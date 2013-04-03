ce2439f702c26d92fc256dd4284fd6a5b9e00560
Eran Raskansky <raskanskyz@gmail.com>
2013-04-03 10:47:03 +0300

    Added documentations

--- abc/abc.c
+++ abc/abc.c
@@ -1,3 +1,11 @@
+/*
+ This program takes a String as input and outputs a "trimmed" version
+ of the string. If the string contains three or more lexicographically adjacent
+ characters the 'printArray' function will trim the string and return a shorter version
+ of the string. For example, if the given string is "abcfjmnopx" this program will return
+ "a-cfjm-px".
+ */
+
 #include <stdio.h>
 #define MAX 1000
 
@@ -13,6 +21,15 @@ int main()
 	return 0;
 }/*main*/
 
+/*
+ The 'printArray' function iterates through the given string until
+ the character is '\0'. It first checks whether to characters are
+ lexicographically adjacent, and if so it will check the character
+ after them, while they are adjacent, an inner loop "trims" the characters
+ by running a pointer to the next character, once the sequence is not in order,
+ the function will stop trimming, add a '-' and the last character in sequence
+ and move on to search the rest of the string.
+*/
 void printArray(char a[])
 	{
 	int i;
